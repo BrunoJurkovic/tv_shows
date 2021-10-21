7bf37f9fcfc700f629744db87bba8d59c837f8f9
Bruno Jurkovic <bruno.jurkovic1@gmail.com>
2021-10-21 15:35:11 +0200

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.tv_shows
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:tv_shows/app/app.dart';
+import 'package:tv_shows/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
