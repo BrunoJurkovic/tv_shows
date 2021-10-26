792f86228fb6433d662e8379083a6cf853e1e3af
Bruno Jurkovic <bruno.jurkovic1@gmail.com>
2021-10-26 22:07:51 +0200

    Fetching shows

--- lib/ui/views/shows/shows_page.dart
+++ lib/ui/views/shows/shows_page.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:tv_shows/business_logic/viewmodels/login_viewmodel.dart';
+import 'package:tv_shows/business_logic/viewmodels/shows_viemodel.dart';
 import 'package:tv_shows/services/service_locator.dart';
 import 'package:tv_shows/ui/views/login/login_page.dart';
 import 'package:tv_shows/ui/widgets/shows/app_bar.dart';
 
-class ShowsPage extends StatelessWidget {
-  ShowsPage({Key? key}) : super(key: key);
+class ShowsPage extends StatefulWidget {
+  const ShowsPage({Key? key}) : super(key: key);
 
+  @override
+  State<ShowsPage> createState() => _ShowsPageState();
+}
+
+class _ShowsPageState extends State<ShowsPage> {
   final _loginViewModel = serviceLocator<LoginViewModel>();
+  final _showsViewModel = serviceLocator<ShowsViewModel>();
+
+  @override
+  void initState() {
+    Future.delayed(Duration.zero, _showsViewModel.fetchShows);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +38,9 @@ class ShowsPage extends StatelessWidget {
           );
         },
       ),
+      body: ListView.builder(itemBuilder: (ctx, i) {
+        return Container();
+      }),
     );
   }
 }
