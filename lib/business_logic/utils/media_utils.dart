0958f3848acd40b7ea08d3770b85b89d4123ec27
Bruno Jurkovic <bruno.jurkovic1@gmail.com>
2021-10-28 18:38:53 +0200

    Episode and media utils

---
+++ lib/business_logic/utils/media_utils.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart';
+import 'package:tv_shows/services/http/http_service.dart';
+
+class MediaResponse {
+  const MediaResponse({required this.mediaId});
+
+  factory MediaResponse.fromMap(Map<String, dynamic> map) {
+    return MediaResponse(
+      // ignore: avoid_dynamic_calls
+      mediaId: map['data']['mediaId'] as String,
+    );
+  }
+
+  final String mediaId;
+}
+
+class MediaRequest extends BaseHttpPostRequest {
+  const MediaRequest(this.formData) : super(endpoint: '/api/media');
+
+  final FormData formData;
+
+  @override
+  Map<String, dynamic> toMap() {
+    // ignore: unnecessary_cast
+    return {
+      'file': formData,
+    } as Map<String, dynamic>;
+  }
+}
